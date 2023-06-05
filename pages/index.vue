718d097773d5c0b5e39fd8f7c4f9c646d2b9b88c
frasza <zan.fraas@gmail.com>
2023-06-05 20:15:22 +0200

    Add reproduction for store bug

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div class="flex w-full justify-center">
-    <NuxtLink :to="{ name: 'nested-id', params: { id: 1 } }">
-      Bug report
+    <NuxtLink class="mt-10 text-xl text-red-800" :to="{ name: 'nested-id', params: { id: 1 } }">
+      Go to the Bug section
     </NuxtLink>
   </div>
 </template>
